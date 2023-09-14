539544a95839e67307a8fb3f43e7678388121520
Gxorge <github@ghott.me>
2023-09-14 21:31:08 +0100

    POS admin permissions

--- Operators/OperatorBoolPermission.cs
+++ Operators/OperatorBoolPermission.cs
@@ -14,22 +14,39 @@ public enum OperatorBoolPermission
     [PermissionPromptName("Hotshot")]
     POS_Hotshot_Access = 20,
 
-    [PermissionPromptName("Item Modifications")]
-    POS_ItemMod_Access = 30,
     [PermissionPromptName("Item Void")]
-    POS_ItemMod_ItemVoid = 31,
+    POS_ItemMod_ItemVoid = 30,
 
-    [PermissionPromptName("Transaction Modifications")]
-    POS_TransMod_Access = 40,
     [PermissionPromptName("Transaction Void")]
-    POS_TransMod_TransVoid = 41,
+    POS_TransMod_TransVoid = 40,
 
     [PermissionPromptName("Open Register")]
     POS_Admin_OpenRegister = 90,
     [PermissionPromptName("Close Register")]
     POS_Admin_CloseRegister = 91,
+
     [PermissionPromptName("Loan / Declare Opening Float")]
-    POS_Admin_Loan = 92,
+    POS_Admin_CashManagement_Loan = 92,
+    [PermissionPromptName("Cash Pickup")]
+    POS_Admin_CashManagement_Pickup = 93,
+    [PermissionPromptName("Cash Spotcheck")]
+    POS_Admin_CashManagement_Spotcheck = 94,
+    [PermissionPromptName("Petty Cash In")]
+    POS_Admin_CashManagement_PettyIn = 95,
+    [PermissionPromptName("Petty Cash Out")]
+    POS_Admin_CashManagement_PettyOut = 96,
+
+    [PermissionPromptName("Receipt Reprint")]
+    POS_Admin_ReceiptReprint = 97,
+    [PermissionPromptName("Postvoid")]
+    POS_Admin_PostVoid = 98,
+    [PermissionPromptName("No Sale")]
+    POS_Admin_NoSale = 99,
+    [PermissionPromptName("Training Mode")]
+    POS_Admin_Training = 100,
+    [PermissionPromptName("X Read")]
+    POS_Admin_XRead = 101,
+
 
     BO_Access = 1000
 }

5088dedb0e90df1eb1f9af59e8086f1732a2d628
Gxorge <github@ghott.me>
2024-08-15 01:14:30 +0100

    squareup helper

---
+++ Helpers/SquareUpHelper.cs
@@ -0,0 +1,24 @@
+﻿using Square.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT_COMMONS.Helpers;
+
+public class SquareUpHelper
+{
+
+    public static long ConvertFloatToSquareLong(float value)
+    {
+        return long.Parse(value.ToString("0.00").Replace(".", string.Empty));
+    }
+
+    public static float ConvertSquareLongToFloat(long value)
+    {
+        string amt = value.ToString();
+        amt = amt.Insert(amt.Length - 2, ".");
+        return float.Parse(amt);
+    }
+}
